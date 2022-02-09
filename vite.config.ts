6ab9241a08a1e8d376e7c36841bc6d50587e3107
Seyit <seyitbu1111@gmail.com>
2022-02-09 12:21:53 +0500

    chore(project): fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default () => {
 
     server: {
       port: Number(process.env.PORT) || 3000,
-      host: process.env.HOST || 'http://127.0.0.1',
+      host: process.env.HOST || '127.0.0.1',
 
       proxy: {
         '/api': {
